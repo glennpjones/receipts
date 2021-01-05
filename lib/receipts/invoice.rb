08481516129eaaa9577c75d20c3f2753da721610
Glenn Jones <glenn@glennjon.es>
2021-01-05 18:31:12 +0100

    Adjust layout

--- lib/receipts/invoice.rb
+++ lib/receipts/invoice.rb
@@ -31,7 +31,7 @@ module Receipts
       end
 
       def default_subheading
-        "INVOICE #%{id}"
+        "%{id}"
       end
 
       def setup_fonts
@@ -55,67 +55,53 @@ module Receipts
         logo = company[:logo]
 
         if logo.nil?
-          move_down 32
+          move_down 40
         elsif logo.is_a?(String)
-          image open(logo), height: 32
+          image open(logo), height: 40
         else
-          image logo, height: 32
+          image logo, height: 40
         end
 
         move_down 8
-        label (subheading % {id: id})
+        # label (subheading % {id: id}) # disable for now
 
-        move_down 10
+        move_down 50
 
         # Cache the Y value so we can have both boxes at the same height
         top = y
         bounding_box([0, y], width: 200) do
-          label "BILL TO"
-
-          move_down 5
-          text_box bill_to, at: [0, cursor], width: 200, height: 75, inline_format: true, size: 10, leading: 4, overflow: :shrink_to_fit
-
+          text_box bill_to, at: [0, cursor], width: 200, height: 150, inline_format: true, size: 12, leading: 4, overflow: :shrink_to_fit
         end
 
-        bounding_box([250, top], width: 200) do
-          label "INVOICE DATE"
+        bounding_box([265, top], width: 100) do
+          text ["Factuur", "Factuurdatum", "Status"].join("\n"), size: 12, leading: 4
+        end
 
-          move_down 5
-          text issue_date.to_s, inline_format: true, size: 12, leading: 4
-
-          move_down 10
-          label "DUE DATE"
-
-          move_down 5
-          text due_date.to_s, inline_format: true, size: 12, leading: 4
-
-          move_down 10
-          label "STATUS"
-
-          move_down 5
-          text status, inline_format: true, size: 12, leading: 4
+        bounding_box([360, top], width: 100) do
+          text [(subheading % {id: id}), issue_date.to_s, status].join("\n"), size: 12, leading: 4, inline_format: true
         end
       end
 
       def charge_details
-        move_down 30
+        move_down 75
 
         borders = line_items.length - 2
 
         table(line_items, width: bounds.width, cell_style: { border_color: 'cccccc', inline_format: true }) do
           cells.padding = 12
           cells.borders = []
+          column(1).style(align: :right)
           row(0..borders).borders = [:bottom]
         end
       end
 
       def footer
-        move_down 30
+        move_down 60
         text message, inline_format: true, size: 12, leading: 4
 
         move_down 30
         text company.fetch(:name), inline_format: true
-        text "<color rgb='888888'>#{company.fetch(:address)}</color>", inline_format: true
+        text company.fetch(:address), inline_format: true
       end
 
       def label(text)
